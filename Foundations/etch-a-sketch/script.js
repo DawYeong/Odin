e708fd8874b23b082d76811fd361aa0cdee93101
Benny Lam <bennylam6@gmail.com>
2024-04-29 15:18:55 -0400

    Add button to prompt user to generate a N sized grid

--- Foundations/etch-a-sketch/script.js
+++ Foundations/etch-a-sketch/script.js
@@ -1,7 +1,13 @@
 const container = document.querySelector(".container");
-console.log(container);
+
+const newGridButton = document.createElement("button");
+newGridButton.className = "new-grid";
+newGridButton.innerText = "Generate Grid";
+container.appendChild(newGridButton);
 
 const createGrid = (size) => {
+  const grid = document.createElement("div");
+  grid.className = "grid";
   for (let i = 0; i < size; i++) {
     const row = document.createElement("div");
     row.className = "row";
@@ -12,6 +18,46 @@ const createGrid = (size) => {
       row.appendChild(box);
     }
 
-    container.appendChild(row);
+    grid.appendChild(row);
   }
+
+  container.appendChild(grid);
 };
+
+newGridButton.addEventListener("click", function (e) {
+  let promptAnswer = "";
+  let newGridSize = 0;
+  while (true) {
+    promptAnswer = prompt("Enter size of new grid (max 100):");
+
+    if (!promptAnswer) return;
+    promptAnswer = promptAnswer.trim();
+
+    newGridSize = parseInt(promptAnswer);
+    if (!newGridSize || newGridSize != promptAnswer) {
+      alert("Please enter a valid integer!");
+      continue;
+    }
+
+    if (newGridSize > 100) {
+      alert("Please enter an integer 100 or smaller!");
+      continue;
+    }
+
+    break;
+  }
+
+  // remove existing grid
+  const currentGrid = document.querySelector(".grid");
+  if (currentGrid) {
+    currentGrid.remove();
+  }
+
+  createGrid(newGridSize);
+});
+
+container.addEventListener("mouseover", function (e) {
+  if (e.target.className === "box") {
+    e.target.style.backgroundColor = "black";
+  }
+});
