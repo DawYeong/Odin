84b6bc1029997871a554af1847d67ff5ad8eedb8
Benny Lam <bennylam6@gmail.com>
2024-05-22 23:45:55 -0400

    Add event handlers to toggle between projects

--- JavaScript/todo-list/src/modules/DisplayHandler.js
+++ JavaScript/todo-list/src/modules/DisplayHandler.js
@@ -7,6 +7,8 @@ export default class DisplayHandler {
   static #projects = document.querySelector("div.projects");
   static #header = document.querySelector("div.header");
   static #mainContent = document.querySelector("div.main-content");
+  static #sideMenu = document.querySelector("div.side-menu");
+  static #navBtn = document.querySelector(".nav-btn");
 
   // init => set up everything => event listeners and things
   static init() {
@@ -47,33 +49,65 @@ export default class DisplayHandler {
   }
 
   static #initPageElements() {
-    DisplayHandler.#initHeader();
     DisplayHandler.displayProjects();
     DisplayHandler.displayTasks();
   }
 
-  static #initHeader() {
-    DisplayHandler.#header.appendChild(
-      createElement("button", "side-menu-toggle", "", "Toggle")
-    );
-    DisplayHandler.#header.appendChild(
-      createElement("h1", "", "", "Todo List")
-    );
-  }
-
   static #createProjectItem(project) {
-    const projectItem = createElement("div", "project-item", "", "");
+    const projectItem = createElement("div", "project-item", "", "", {
+      projectId: project.getId(),
+    });
     // add more elements
-    projectItem.appendChild(createElement("p", "", "", project.getName()));
+    projectItem.appendChild(createElement("div", "", "", project.getName()));
     return projectItem;
   }
 
   static #createTaskItem(task) {
     const taskItem = createElement("div", "task-item", "", "");
     // add more elements
+    taskItem.appendChild(
+      createElement("input", "", "", "", { type: "checkbox" })
+    );
     taskItem.appendChild(createElement("p", "", "", task.getTitle()));
     return taskItem;
   }
 
-  static #initEventListeners() {}
+  static #initEventListeners() {
+    document
+      .querySelectorAll("button.side-menu-toggle")
+      .forEach((toggleBtn) => {
+        toggleBtn.addEventListener("click", () => {
+          DisplayHandler.#toggleSideMenu();
+        });
+      });
+
+    document.querySelector(".task-items").addEventListener("click", (e) => {
+      if (e.target.nodeName === "INPUT") {
+        console.dir(e.target.checked);
+      }
+    });
+
+    document.querySelector(".projects").addEventListener("click", (e) => {
+      const projectId = DisplayHandler.#getProjectIdFromEvent(e.target);
+
+      if (projectId != DisplayHandler.todo.getActiveProject()) {
+        DisplayHandler.todo.setActiveProject(projectId);
+        DisplayHandler.displayTasks();
+      }
+    });
+  }
+
+  static #toggleSideMenu() {
+    DisplayHandler.#sideMenu.classList.toggle("open");
+    DisplayHandler.#navBtn.classList.toggle("close");
+  }
+
+  static #getProjectIdFromEvent(event) {
+    let currEvent = event;
+    while (currEvent.className != "project-item") {
+      currEvent = currEvent.parentNode;
+    }
+
+    return currEvent.attributes["projectId"].value;
+  }
 }
