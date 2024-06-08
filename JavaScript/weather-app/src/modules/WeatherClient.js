241dc56ca1ec6d11cab24ae0c65daf5dbac25724
Benny Lam <bennylam6@gmail.com>
2024-06-07 22:57:38 -0400

    Add units to data and change variable names

--- JavaScript/weather-app/src/modules/WeatherClient.js
+++ JavaScript/weather-app/src/modules/WeatherClient.js
@@ -15,14 +15,14 @@ const DAYS = 3; // this is the limit for the free API tier
 export class WeatherClient {
   #key;
   #currentData;
-  #isC;
-  constructor(key) {
+  #isFahrenheit;
+  constructor(key, isFahrenheit) {
     this.#key = key;
-    this.#isC = true;
+    this.#isFahrenheit = isFahrenheit;
     this.#currentData = {};
   }
 
-  async getLocationForecast(location, isCelsius) {
+  async fetchForecast(location) {
     const url = `${BASEURL}/forecast.json?key=${
       this.#key
     }&q=${location}&days=${DAYS}&alerts=no`;
@@ -30,10 +30,9 @@ export class WeatherClient {
     const response = await fetch(url);
     const jsonData = await response.json();
     this.#processForecastJSONData(jsonData);
-    return this.getForecastData(isCelsius);
   }
 
-  getForecastData(isCelsius) {
+  getForecastData() {
     const currentKeys = new Set(Object.keys(this.#currentData));
 
     if (
@@ -43,7 +42,7 @@ export class WeatherClient {
     )
       throw new Error("Current data is not forecast data");
 
-    const tempType = isCelsius ? "c" : "f";
+    const tempType = this.#isFahrenheit ? "f" : "c";
 
     return {
       current: {
@@ -53,12 +52,12 @@ export class WeatherClient {
       forecast: this.#currentData["forecast"].map((day, i) => {
         return { ...day, ...this.#currentData[tempType]["forecast"][i] };
       }),
+      location: this.#currentData["location"],
     };
   }
 
   #processForecastJSONData(data) {
     if (this.#checkError(data)) {
-      console.log(data["error"]);
       throw new Error(data["error"]["message"]);
     }
 
@@ -74,14 +73,11 @@ export class WeatherClient {
     this.#processCurrentData(data["current"], processedData);
 
     this.#processForecastData(data["forecast"], processedData);
-    console.log("processed Data:");
-    console.log(processedData);
 
     this.#setCurrentData(processedData);
   }
 
   #processLocationData(locData, res) {
-    console.log(locData);
     res["location"] = `${locData["name"]}, ${locData["country"]}`;
   }
 
@@ -89,17 +85,17 @@ export class WeatherClient {
     res["current"] = {
       date: format(currData["last_updated"], "EEEE, MMMM d"),
       icon: currData["condition"]["icon"],
-      precip_mm: currData["precip_mm"],
-      wind: currData["wind_kph"],
-      humidity: currData["humidity"],
+      precip_mm: `${currData["precip_mm"]} mm`,
+      wind: `${currData["wind_kph"]} km/h`,
+      humidity: `${currData["humidity"]} %`,
     };
     res["c"]["current"] = {
-      feelslike: currData["feelslike_c"],
-      temp: currData["temp_c"],
+      feelslike: `${currData["feelslike_c"]} °C`,
+      temp: `${currData["temp_c"]} °C`,
     };
     res["f"]["current"] = {
-      feelslike: currData["feelslike_f"],
-      temp: currData["temp_f"],
+      feelslike: `${currData["feelslike_f"]} °F`,
+      temp: `${currData["temp_f"]} °F`,
     };
   }
 
@@ -108,29 +104,28 @@ export class WeatherClient {
     res["c"]["forecast"] = [];
     res["f"]["forecast"] = [];
 
-    console.log(fcData["forecastday"]);
     fcData["forecastday"].forEach((forecast) => {
       const day = forecast["day"];
 
       res["forecast"].push({
         date: format(forecast["date"], "EEEE, MMMM d"),
         icon: day["condition"]["icon"],
-        humidity: day["avghumidity"],
-        precip: day["totalprecip_mm"],
-        wind: day["maxwind_kph"],
-        pop: day["daily_chance_of_rain"],
+        humidity: `${day["avghumidity"]} %`,
+        precip: `${day["totalprecip_mm"]} mm`,
+        wind: `${day["maxwind_kph"]} km/h`,
+        pop: `${day["daily_chance_of_rain"]} %`,
       });
 
       res["c"]["forecast"].push({
-        mintemp: day["mintemp_c"],
-        maxtemp: day["maxtemp_c"],
-        avgtemp: day["avgtemp_c"],
+        mintemp: `${day["mintemp_c"]} °C`,
+        maxtemp: `${day["maxtemp_c"]} °C`,
+        avgtemp: `${day["avgtemp_c"]} °C`,
       });
 
       res["f"]["forecast"].push({
-        mintemp: day["mintemp_f"],
-        maxtemp: day["maxtemp_f"],
-        avgtemp: day["avgtemp_f"],
+        mintemp: `${day["mintemp_f"]} °F`,
+        maxtemp: `${day["maxtemp_f"]} °F`,
+        avgtemp: `${day["avgtemp_f"]} °F`,
       });
     });
   }
@@ -141,5 +136,9 @@ export class WeatherClient {
 
   #setCurrentData(data) {
     this.#currentData = data;
+  }
+
+  setIsFahrenheit(isFahrenheit) {
+    this.#isFahrenheit = isFahrenheit;
   }
 }
