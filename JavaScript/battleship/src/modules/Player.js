3331828b098512fa47929c59de53efeaafef18cb
Benny Lam <bennylam6@gmail.com>
2024-06-20 19:11:10 -0400

    Update player object to be able to place ships and check state

--- JavaScript/battleship/src/modules/Player.js
+++ JavaScript/battleship/src/modules/Player.js
@@ -1,12 +1,41 @@
 import { Gameboard } from "./Gameboard";
 
-class Player {
-  constructor(name) {
-    this.name = name;
+export class Player {
+  static shipLengths = [5, 4, 3, 3, 2];
+
+  constructor() {
     this.gameBoard = new Gameboard();
+    this.numShips = 0;
+  }
+
+  placeShip(row, col, isHorizontal) {
+    if (this.isAllShipsPlaced()) return false;
+
+    if (
+      this.gameBoard.placeShip(
+        row,
+        col,
+        Player.shipLengths[this.numShips],
+        isHorizontal
+      ) != null
+    ) {
+      this.numShips += 1;
+      return true;
+    }
+
+    return false;
   }
 
   fire(row, col, enemyGB) {
     return enemyGB.receiveAttack(row, col);
+  }
+
+  getCurrentShipLength() {
+    if (this.isAllShipsPlaced()) return -1;
+    return Player.shipLengths[this.numShips];
+  }
+
+  isAllShipsPlaced() {
+    return this.numShips === Player.shipLengths.length;
   }
 }
